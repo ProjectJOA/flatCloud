73e59f85abd9fb3c43fcc7099b186f16d668e101
projectjoa <openprojectjoa@gmail.com>
2021-05-13 13:07:07 +0900

    오타수정

--- instance/my_ec2instance.py
+++ instance/my_ec2instance.py
@@ -42,7 +42,7 @@ def get_ssh_access():
 			objInfo = get_simple_ec2instance_info(oneObj)
 			instArr = objInfo.split(" : ")
 			if instArr[2] != 'no public ip':
-				print("ssh -i ~/.ssh/vini_key.pem ec2_user@"+instArr[2])
+				print("ssh -i ~/.ssh/vini_key.pem ec2-user@"+instArr[2])
 			print("")
 	return "success"
 
